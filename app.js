f77c2b2061a8e39dbeecfac57fe7b901621e9c66
Victor Mu <vic.mgs@gmail.com>
2016-09-30 12:18:05 -0700

    Complete exercises for components

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// TODO
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+  onEnter() {
+    this.setState({
+      done: true
+    });
+  }
+  onLeave() {
+    this.setState({
+      done: false
+    });
+  }
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.onEnter.bind(this)} onMouseLeave={this.onLeave.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GList = (props) => (
+  <ul>
+    {props.items.map(todo =>
+      <TodoListItem item={todo} />
+    )}
+  </ul>
+);
+
+var FullList = () => (
+  <div>
+    <p>My List</p>
+    <GList items={['apple','poo']}/>
+    <GList items={['apple1','peach1','pear1']}/>
+  </div>
+);
+
+
+
+
+
+ReactDOM.render(<FullList />, document.getElementById("app"));
